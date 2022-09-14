621946bfe4c8d2fd75c1965b3168b790d14e39bc
Mayara Bispo <mayarareginabisp@gmail.com>
2022-09-14 16:27:26 -0300

    Corrigindo links para urlIos

--- src/data/docsList.js
+++ src/data/docsList.js
@@ -9,175 +9,175 @@ const docsList = [
       id: 2,
       title: '02 - Declaração de Vida Funcional',
       urlAndroid: '02-declaracao-vida-funcional.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/02-declaracao-vida-funcional.pdf')
   },
   {
       id: 3,
       title: '03 - Declaração de Acúmulo de Cargo Público',
       urlAndroid: '03-declaracao-acumulo-cargo-publico.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/03-declaracao-acumulo-cargo-publico.pdf')
   },
   {
     id: 4,
     title: '04 - Compatibilidade de Horário - Cargo Privado',
     urlAndroid: '04-compatibilidade-horario-cargo-privado.pdf',
-    urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+    urlIos: require('../docs-servidores/04-compatibilidade-horario-cargo-privado.pdf')
   },
   {
     id: 5,
     title: '05 - Declaração de Parentesco',
     urlAndroid: '05-declaracao-parentesco.pdf',
-    urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+    urlIos: require('../docs-servidores/05-declaracao-parentesco.pdf')
   },
   {
     id: 6,
     title: '06 - Ficha de Identificação do Servidor',
     urlAndroid: '06-ficha-identificacao-servidor.pdf',
-    urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+    urlIos: require('../docs-servidores/06-ficha-identificacao-servidor.pdf')
   },
   {
       id: 7,
       title: '07 - Termo de Compromisso - Documentos Pendentes',
       urlAndroid: '07-termo-compromisso-documentos-pendentes.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/07-termo-compromisso-documentos-pendentes.pdf')
   },
   {
       id: 8,
       title: '09 - Abertura de Conta-Salário Nubia',
       urlAndroid: '09-abertura-conta-salario-nubia.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/09-abertura-conta-salario-nubia.pdf')
   },
   {
       id: 9,
       title: '10 - Protocolo de Recebimento de Documentação - Processo Seletivo',
       urlAndroid: '10-protocolo-recebimento-documentacao-processo-seletivo.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/10-protocolo-recebimento-documentacao-processo-seletivo.pdf')
     },
     {
       id: 10,
       title: '11 - Protocolo de Recebimento de Documentação - Servidores em Geral',
       urlAndroid: '11-protocolo-recebimento-documentacao-servidores-geral.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/11-protocolo-recebimento-documentacao-servidores-geral.pdf')
     },
     {
       id: 11,
       title: '13 - Número da Conta-Salário',
       urlAndroid: '13-numero-conta-salario.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/13-numero-conta-salario.pdf')
     },
     {
       id: 12,
       title: '14 - Requerimento de Crachá',
       urlAndroid: '14-requerimento-cracha.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/14-requerimento-cracha.pdf')
     },
     {
       id: 13,
       title: '15 - Requerimento de Exoneração',
       urlAndroid: '15-requerimento-exoneracao.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/15-requerimento-exoneracao.pdf')
     },
     {
       id: 14,
       title: '16 - Requerimento de Falta Abonada Anual',
       urlAndroid: '16-requerimento-falta-abonada-anual.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/16-requerimento-falta-abonada-anual.pdf')
     },
     {
       id: 15,
       title: '17 - Requerimento de Férias e Licença-Prêmio',
       urlAndroid: '17-requerimento-ferias-licenca-premio.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/17-requerimento-ferias-licenca-premio.pdf')
     },
     {
       id: 16,
       title: '18 - Requerimento de Licença Maternidade',
       urlAndroid: '18-requerimento-licenca-maternidade.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/18-requerimento-licenca-maternidade.pdf')
     },
     {
       id: 17,
       title: '19 - Requerimento de Licença para Tratar de Assuntos Particulares',
       urlAndroid: '19-requerimento-licenca-para-tratar-assuntos-particulares.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/19-requerimento-licenca-para-tratar-assuntos-particulares.pdf')
     },
     {
       id: 18,
       title: '20 - Solicitação de Recesso Estagiários',
       urlAndroid: '20-solicitacao-recesso-estagiarios.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/20-solicitacao-recesso-estagiarios.pdf')
     },
     {
       id: 19,
       title: '21 - Declaração de Comparecimento Nubia',
       urlAndroid: '21-declaracao-comparecimento-nubia.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/21-declaracao-comparecimento-nubia.pdf')
     },
     {
       id: 20,
       title: '22 - Solicitação de Declaração de Margem',
       urlAndroid: '22-solicitacao-declaracao-margem.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/22-solicitacao-declaracao-margem.pdf')
     },
     {
       id: 21,
       title: '24 - Requerimento Protocolo',
       urlAndroid: '24-requerimento-protocolo.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/24-requerimento-protocolo.pdf')
     },
     {
       id: 22,
       title: '25 - Prorrogação de Efetivo Exercício',
       urlAndroid: '25-prorrogacao-efetivo-exercicio.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/25-prorrogacao-efetivo-exercicio.pdf')
     },
     {
       id: 23,
       title: '26 - Formulário de Utilização de Banco de Horas',
       urlAndroid: '26-formulario-utilizacao-banco-horas.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/26-formulario-utilizacao-banco-horas.pdf')
     },
     {
       id: 24,
       title: '27 - Requerimento para Evolução Funcional',
       urlAndroid: '27-requerimento-evolucao-funcional.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/27-requerimento-evolucao-funcional.pdf')
     },
     {
       id: 25,
       title: '28 - Gratificação por Trabalho com Risco de Vida ou à Saúde',
       urlAndroid: '28-gratificacao-trabalho-risco-vida-ou-saude.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/28-gratificacao-trabalho-risco-vida-ou-saude.pdf')
     },
     {
       id: 26,
       title: '29 - Inclusão Cálculo Contribuição Itapeviprev',
       urlAndroid: '29-inclusao-calculo-contribuicao-itapeviprev.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/29-inclusao-calculo-contribuicao-itapeviprev.pdf')
     },
     {
       id: 27,
       title: '30 - Requerimento de Contagem de Banco de Horas',
       urlAndroid: '30-requerimento-contagem-banco-horas.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/30-requerimento-contagem-banco-horas.pdf')
     },
     {
       id: 28,
       title: '31 - Formulário de Justificativa',
       urlAndroid: '31-formulario-justificativa.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/31-formulario-justificativa.pdf')
     },
     {
       id: 29,
       title: '32 - Pedido de Desincompatibilização - Servidor Efetivo',
       urlAndroid: '32-pedido-desincompatibilizacao-servidor-efetivo.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/32-pedido-desincompatibilizacao-servidor-efetivo.pdf')
     },
     {
       id: 30,
       title: '33 - Requerimento de Licença Prêmio In Pecúnia',
       urlAndroid: '33-requerimento-licenca-premio-in-pecunia.pdf',
-      urlIos: require('../docs-servidores/01-declaracao-bens.pdf')
+      urlIos: require('../docs-servidores/33-requerimento-licenca-premio-in-pecunia.pdf')
     },
   ];
 
